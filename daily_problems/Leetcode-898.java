94c3780cc81440153ab65bf4d92ef4545dda0f78
PriyanshuPritam <priyanshupritam123@gmail.com>
2025-08-20 11:35:06 +0530

    Day 2

---
+++ daily_problems/Leetcode-898.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int subarrayBitwiseORs(int[] arr) {
+        Set<Integer> res=new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            res.add(arr[i]);
+            for(int j=i-1;j>=0;j--){
+                if(arr[j]==(arr[j]|arr[i]))break;
+                arr[j] |= arr[i];
+                res.add(arr[j]);
+            }
+        }
+        return res.size();
+    }
+}
