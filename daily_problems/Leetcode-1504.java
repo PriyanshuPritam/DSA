fe7537e71d247129d17c6a3d6946fb4ef00749af
Priyanshu Pritam <122999958+PriyanshuPritam@users.noreply.github.com>
2025-08-21 10:56:49 +0530

    Update Leetcode-1504.java

--- daily_problems/Leetcode-1504.java
+++ daily_problems/Leetcode-1504.java
@@ -1,23 +1,28 @@
 class Solution {
     public int numSubmat(int[][] mat) {
-        int rows=mat.length;
-        int cols=mat[0].length;
-        int[]heights=new int[cols];
-        int result=0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(mat[i][j]==0)heights[j]=0;
-                else heights[j]+=1;
+        int n=mat.length;
+        int m=mat[0].length;
+
+        int[][] width=new int[n][m];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0)
+                width[i][j]=0;
+                else
+                width[i][j]=(i==0 ? 1: width[i-1][j]+1);
             }
-            for(int j=0;j<cols;j++){
-                int minHeight=Integer.MAX_VALUE;
-                for(int k=j;k>=0;k--){
-                    if(heights[k]==0)break;
-                    minHeight=Math.min(minHeight,heights[k]);
-                    result+=minHeight;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int min=width[i][j];
+                for(int k=j;k>=0 && min>0;k--){
+                    min = Math.min(min,width[i][k]);
+                    ans += min;
                 }
             }
         }
-        return result;
+        return ans;
     }
 }
